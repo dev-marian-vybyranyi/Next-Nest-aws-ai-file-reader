4b89914ef5ea93eff93f13dcb9f3e97b97974719
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-03-23 11:10:28 +0200

    Allow all CORS origins by setting the origin to a wildcard.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://*.vercel.app'],
+    origin: ['*'],
+    // origin: ['http://localhost:3000', 'https://*.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
@@ -16,6 +17,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3001);
   console.log(`Server running on http://localhost:3001`);
-
 }
 bootstrap();
